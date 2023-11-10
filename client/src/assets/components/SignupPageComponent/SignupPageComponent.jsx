4393528b8ab4f01f389eb42e56305e97fdbdbc72
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2023-11-11 01:20:33 +0330

    OTP modal added!

--- client/src/assets/components/SignupPageComponent/SignupPageComponent.jsx
+++ client/src/assets/components/SignupPageComponent/SignupPageComponent.jsx
@@ -4,13 +4,19 @@ import { useEffect, useState } from 'react';
 import { EmailChecker, PhoneChecker } from '../REGEX/Regex';
 
 function SignupPageComponent() {
-    const squaresArray = Array.from(Array(260).keys())
+    // const squaresArray = Array.from(Array(260).keys())
 
-    const [userEmail, setUserEmail] = useState(null)
-    const [userPhone, setUserPhone] = useState(null)
+    const [formFlag, setFormFlag] = useState(false)
+    const [userOTP, setuUserOTP] = useState("")
+    const [username, setUsername] = useState("")
+    const [userPass, setUserPass] = useState("")
+    const [userConfirmPass, setUserConfirmPass] = useState("")
+    const [userEmail, setUserEmail] = useState("")
+    const [userPhone, setUserPhone] = useState("")
     const [emailOrPhone, setEmailOrPhone] = useState("")
+    const [showModal, setShowModal] = useState(false)
 
-    const FormChecker = (inp) => {
+    const EmailPhoneValidator = (inp) => {
         if (EmailChecker(inp) | PhoneChecker(inp)) {
             if (EmailChecker(inp)) {
                 setUserEmail(inp)
@@ -19,32 +25,43 @@ function SignupPageComponent() {
                 setUserPhone(inp)
                 setUserEmail(null)
             }
+            return true
         } else {
-            alert("invalid information!")
+            alert("")
+            return false
         }
     }
 
-    useEffect(() => {
-        if (userEmail) {
-            console.log("email : ", userEmail, userPhone)
-        } else if(userPhone) {
-            console.log("phone : ", userPhone, userEmail)
+    const FormChecker = () => {
+        if (EmailPhoneValidator(emailOrPhone)) {
+            setFormFlag(true)
         }
-    }, [userEmail, userPhone])
+    }
+
+    const FormSender = () => {
+        console.log(username, emailOrPhone, userEmail, userPhone, userPass, userConfirmPass)
+    }
+
+    useEffect(() => {
+        if (formFlag) {
+            FormSender()
+            setShowModal(true)
+        }
+    }, [formFlag])
 
     return (
         <>
             <section className="signup-page-container">
 
-                {
+                {/* {
                     squaresArray.map((sq, i) => (
                         <span key={i} className="bg-square"></span>
                     ))
-                }
+                } */}
                 <div className="sign-up">
 
                     <img src="./../../../../public/general_images/logo.png" alt="" />
-                    <div className="content">
+                    <div className="form-container">
 
                         <h2 className="form-title">signup</h2>
 
@@ -52,13 +69,18 @@ function SignupPageComponent() {
 
                             <div className="inputBox">
 
-                                <input type="password" required /> <i>Username</i>
+                                <input type="text"
+                                    required
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)}
+                                /><i>Username</i>
 
                             </div>
 
                             <div className="inputBox">
 
                                 <input type="text"
+                                    required
                                     value={emailOrPhone}
                                     onChange={(e) => setEmailOrPhone(e.target.value)}
                                 /><i>Email / phone number</i>
@@ -66,7 +88,23 @@ function SignupPageComponent() {
 
                             <div className="inputBox">
 
-                                <input type="password" required /> <i>Password</i>
+                                <input
+                                    type="password"
+                                    required
+                                    value={userPass}
+                                    onChange={e => setUserPass(e.target.value)}
+                                /> <i>Password</i>
+
+                            </div>
+
+                            <div className="inputBox">
+
+                                <input
+                                    type="password"
+                                    required
+                                    value={userConfirmPass}
+                                    onChange={e => setUserConfirmPass(e.target.value)}
+                                /> <i>Password</i>
 
                             </div>
 
@@ -86,6 +124,13 @@ function SignupPageComponent() {
                     </div>
 
                 </div>
+
+                {showModal && <div className="otp-modal-bg">
+                    <div className="otp-modal">
+                        <span>Enter your code</span>
+                        <input className='otp-input' type="text" value={userOTP} onChange={e => setuUserOTP(e.target.value)}/>
+                    </div>
+                </div>}
 
             </section>
         </>
