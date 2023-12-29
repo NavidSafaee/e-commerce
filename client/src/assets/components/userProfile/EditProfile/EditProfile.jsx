d5c1c7f20b05c120375b465e37fc83bc7b9a6807
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2023-12-30 00:19:06 +0330

    profile edit modal added

--- client/src/assets/components/userProfile/EditProfile/EditProfile.jsx
+++ client/src/assets/components/userProfile/EditProfile/EditProfile.jsx
@@ -1,39 +1,68 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import Zoom from '@mui/material/Zoom';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
 import style from './EditProfile.module.scss'
 import AuthContext from '../../Context/AuthContext'
 import { RiEdit2Line } from "react-icons/ri";
 import { PiWarningFill } from "react-icons/pi";
 import { MdVerified } from "react-icons/md";
+import EditModal from './EditModal';
 
 function EditProfile() {
 
+  const [showModal, setShowModal] = useState(false)
+  const [modalType, setModalType] = useState("")
+  const [default_value, setDefault_value] = useState(null)
+  const [userInfo, setUserInfo] = useState({})
   const authContext = useContext(AuthContext)
+
+  const ModalCloser = () => {
+    setShowModal(false)
+  }
+
+  useEffect(() => {
+    setUserInfo(authContext.userInfo)
+  }, [authContext])
+
+  useEffect(() => {
+    if (modalType.length) {
+      setShowModal(true)
+    }
+  }, [modalType])
+
+  useEffect(() => {
+    if (!showModal) {
+      setModalType("")
+    }
+  }, [showModal])
 
   return (
     <section className={style.profileMainSection}>
+      {showModal && <EditModal onHandleModal={ModalCloser} modalType={modalType} default_value={default_value}/>}
       <div className={style.filedsWrapper}>
         <div className={style.editItem}>
           <div className={style.contentSide}>
             <b className={style.boxTitle}>Username</b>
-            <span className={style.boxValue}>{authContext.userInfo.username}</span>
+            <span className={style.boxValue}>{userInfo.username}</span>
           </div>
-          <RiEdit2Line className={style.editBtn} />
+          <RiEdit2Line className={style.editBtn} onClick={() => {setModalType("username"); setDefault_value(userInfo.username)}}/>
         </div>
         <div className={style.editItem}>
           <div className={style.contentSide}>
-            <b className={style.boxTitle}>Email {authContext.userInfo.email && <MdVerified />}</b>
-            <span className={style.boxValue}>{authContext.userInfo.email}</span>
+            {authContext.userInfo.email ? <Tooltip title="verified" placement="right" TransitionComponent={Zoom}><b className={style.boxTitle}>Email <MdVerified /></b></Tooltip> : <b className={style.boxTitle}>Email</b>}
+            <span className={style.boxValue}>{userInfo.email}</span>
           </div>
-          <RiEdit2Line className={style.editBtn} />
-          {!authContext.userInfo.email ? <PiWarningFill className={style.warnIcon} /> : null}
+          <RiEdit2Line className={style.editBtn} onClick={() => {setModalType("email"); setDefault_value(userInfo.email)}}/>
+          {!authContext.userInfo.email ? <Tooltip title="Please fill in this field" placement="top" TransitionComponent={Zoom}><Button><PiWarningFill className={style.warnIcon} /></Button></Tooltip> : null}
         </div>
         <div className={style.editItem}>
           <div className={style.contentSide}>
-            <b className={style.boxTitle}>Phone number {authContext.userInfo.phoneNumber && <MdVerified />}</b>
+            {authContext.userInfo.phoneNumber ? <Tooltip title="verified" placement="right" TransitionComponent={Zoom}><b className={style.boxTitle}>Phone number <MdVerified /></b></Tooltip> : <b className={style.boxTitle}>Phone number</b>}
             <span className={style.boxValue}>{authContext.userInfo.phoneNumber ? authContext.userInfo.phoneNumber : "---"}</span>
           </div>
           <RiEdit2Line className={style.editBtn} />
-          {!authContext.userInfo.phoneNumber ? <PiWarningFill className={style.warnIcon} /> : null}
+          {!authContext.userInfo.phoneNumber ? <Tooltip title="Please fill in this field" placement="top" TransitionComponent={Zoom}><Button><PiWarningFill className={style.warnIcon} /></Button></Tooltip> : null}
         </div>
         <div className={style.editItem}>
           <div className={style.contentSide}>
@@ -41,7 +70,7 @@ function EditProfile() {
             <span className={style.boxValue}>{authContext.userInfo.birthday ? authContext.userInfo.birthday : "---"}</span>
           </div>
           <RiEdit2Line className={style.editBtn} />
-          {!authContext.userInfo.birthday ? <PiWarningFill className={style.warnIcon} /> : null}
+          {!authContext.userInfo.birthday ? <Tooltip title="Please fill in this field" placement="top" TransitionComponent={Zoom}><Button><PiWarningFill className={style.warnIcon} /></Button></Tooltip> : null}
         </div>
         <div className={style.editItem}>
           <div className={style.contentSide}>
@@ -56,7 +85,7 @@ function EditProfile() {
             <span className={style.boxValue}>{authContext.userInfo.address ? authContext.userInfo.address : "---"}</span>
           </div>
           <RiEdit2Line className={style.editBtn} />
-          {!authContext.userInfo.address ? <PiWarningFill className={style.warnIcon} /> : null}
+          {!authContext.userInfo.address ? <Tooltip title="Please fill in this field" placement="top"><Button><PiWarningFill className={style.warnIcon} /></Button></Tooltip> : null}
         </div>
       </div>
     </section>
