b63c2622c3405dd2e3db1764cc09df7dd2701e39
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2023-12-29 19:03:22 +0330

    refactor: convert scss file to module format

--- client/src/assets/components/HeroSlider/HeroSlider.jsx
+++ client/src/assets/components/HeroSlider/HeroSlider.jsx
@@ -1,47 +1,47 @@
 import { useState } from 'react';
 import sliderProducts from "./sliderProductsDetail.json"
-import './HeroSlider.scss'
+import style from './HeroSlider.module.scss'
 
 function HeroSlider() {
 
     const [selectedProductID, setSelectedProductID] = useState(0)
 
     return (
-        <div className='slider-section'>
-            <div className="slider-box">
-                <div className="main-box">
-                    <div className="text-box">
-                        <p className="top-text">
-                            TOP COLLECTIONS 2023
+        <div className={style.slider_section}>
+            <div className={style.slider_box}>
+                <div className={style.main_box}>
+                    <div className={style.text_box}>
+                        <p className={style.top_text}>
+                            TOP COLLECTIONS 2024
                         </p>
-                        <p className='bold-text'>
+                        <p className={style.bold_text}>
                             We Serve Your Dream Furniture!
                         </p>
-                        <p className="off-text">
+                        <p className={style.off_text}>
                             Get 50% off all products
                         </p>
-                        <button className='buy-btn'>SHOP NOW</button>
+                        <button className={style.buy_btn}>SHOP NOW</button>
                     </div>
                     <img
-                        src={`./../../../../public/general_images/slider-imgs/${sliderProducts.products[selectedProductID].imgFileName}`}
+                        src={`/general_images/slider-imgs/${sliderProducts.products[selectedProductID].imgFileName}`}
                         alt="sofa"
                     />
                 </div>
-                <div className="sidebar">
+                <div className={style.sidebar}>
                     {
                         sliderProducts.products.map(product => (
-                            <div className={`item ${(selectedProductID === product.id) && "selected"}`} key={product.id} onClick={() => setSelectedProductID(product.id)}>
-                                <img src={`./../../../../public/general_images/slider-imgs/${product.imgFileName}`} alt="sofa" />
-                                <span className="price">${product.price}</span>
-                                <b className='product-title'>{product.title}</b>
+                            <div className={`${style.item} ${(selectedProductID === product.id) && style.selected}`} key={product.id} onClick={() => setSelectedProductID(product.id)}>
+                                <img src={`/general_images/slider-imgs/${product.imgFileName}`} alt="sofa" />
+                                <span className={style.price}>${product.price}</span>
+                                <b className={style.product_title}>{product.title}</b>
                             </div>
                         ))
                     }
                 </div>
-                <div className="circles-box">
-                    <span className={`circle ${selectedProductID==0 && "focus"}`} onClick={() => setSelectedProductID(0)}></span>
-                    <span className={`circle ${selectedProductID==1 && "focus"}`} onClick={() => setSelectedProductID(1)}></span>
-                    <span className={`circle ${selectedProductID==2 && "focus"}`} onClick={() => setSelectedProductID(2)}></span>
+                <div className={style.circles_box}>
+                    <span className={`${style.circle} ${selectedProductID == 0 && style.focus}`} onClick={() => setSelectedProductID(0)}></span>
+                    <span className={`${style.circle} ${selectedProductID == 1 && style.focus}`} onClick={() => setSelectedProductID(1)}></span>
+                    <span className={`${style.circle} ${selectedProductID == 2 && style.focus}`} onClick={() => setSelectedProductID(2)}></span>
                 </div>
             </div>
         </div>
