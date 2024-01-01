1741a14eeba8b63fc3b792bc67b73b4673383b98
Zahra-gasemi <gasemiz855@gmail.com>
2024-01-02 00:25:50 +0330

    the home tab in the admin panel updated

--- client/src/assets/dashboard/panelPages/home/Home.jsx
+++ client/src/assets/dashboard/panelPages/home/Home.jsx
@@ -3,17 +3,41 @@ import "./Home.css";
 import WidgetSm from "../../components/WidgetLg/WidgetLg";
 import WidgetLg from "../../components/WidgetLg/WidgetLg";
 import Chart from "./../../components/Chart/Chart";
-import { xAxisData } from '../../datas';
-
+import { xAxisData } from "../../datas";
 
 export default function Home() {
   return (
     <div className="home">
-      <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" />
-      <div className="homeWidgets">
-        <WidgetSm />
-        <WidgetLg />
+      <div className="HomeCards">
+        <div className="HomeCard">
+          <div className="HomeCardHeader">
+            <div className="HomeCardHeaderIcon">icon</div>
+            <div className="HomeCardHeaderTitle">totalSale</div>
+          </div>
+          <div className="HomeCardValue">
+            <p id="HomeCardValue">$450000</p>
+          </div>
+        </div>
+        <div className="HomeCard">
+          <div className="HomeCardHeader">
+            <div className="HomeCardHeaderIcon">icon</div>
+            <div className="HomeCardHeaderTitle">totalSale</div>
+          </div>
+          <div className="HomeCardValue">
+            <p id="HomeCardValue">$450000</p>
+          </div>
+        </div>
+        <div className="HomeCard">
+          <div className="HomeCardHeader">
+            <div className="HomeCardHeaderIcon">icon</div>
+            <div className="HomeCardHeaderTitle">totalSale</div>
+          </div>
+          <div className="HomeCardValue">
+            <p id="HomeCardValue">$450000</p>
+          </div>
+        </div>
       </div>
+      <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" className="chartHome" />
     </div>
   );
 }
