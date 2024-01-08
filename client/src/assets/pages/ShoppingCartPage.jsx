fab0dfc225e6a1a7eabbaea4b40498360ebebcd0
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2024-01-08 12:53:34 +0330

    access control implementation for cart and profile

--- client/src/assets/pages/ShoppingCartPage.jsx
+++ client/src/assets/pages/ShoppingCartPage.jsx
@@ -1,15 +1,54 @@
+import { useEffect, useState } from "react"
+import { useLocation, useNavigate } from 'react-router-dom'
 import Footer from "../components/Footer/Footer"
 import ShoppingCart from "../components/ShoppingCart/ShoppingCart"
 import TopBar from "../components/TopBar/TopBar"
 import TopStrip from "../components/TopStrip/TopStrip"
+import baseURL from "../baseURL"
+import PreLoader from "../components/PreLoader/PreLoader"
 
 function ShoppingCartPage() {
+
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const userToken = JSON.parse(localStorage.getItem("userToken"))
+
+    if (userToken) {
+      fetch(`${baseURL}/users/me`, {
+        headers: {
+          Authorization: `Bearer ${userToken.accessToken}`
+        }
+      }).then(res => {
+        return res.json()
+      }).then(userData => {
+        if (userData.role === "CUSTOMER") {
+          setIsLoaded(true)
+        } else {
+          navigate("/access-denied")
+        }
+      })
+    } else {
+      navigate("/access-denied")
+    }
+  }, [pathname])
+
   return (
     <>
-      <TopStrip />
-      <TopBar />
-      <ShoppingCart />
-      <Footer />
+      {
+        isLoaded ?
+          <>
+            <TopStrip />
+            <TopBar />
+            <ShoppingCart />
+            <Footer />
+          </>
+          :
+          <PreLoader />
+      }
     </>
   )
 }
--- client/src/assets/pages/UserProfilePage.jsx
+++ client/src/assets/pages/UserProfilePage.jsx
@@ -4,18 +4,55 @@ import TopBar from "../components/TopBar/TopBar"
 import TopStrip from "../components/TopStrip/TopStrip"
 import Footer from "../components/Footer/Footer"
 import ProfilePageContainer from "../components/userProfile/ProfilePageContainer"
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import baseURL from "../baseURL"
+import PreLoader from "../components/PreLoader/PreLoader"
 
 function UserProfilePage(props) {
 
+    const { pathname } = useLocation()
+    const navigate = useNavigate()
+
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        const userToken = JSON.parse(localStorage.getItem("userToken"))
+
+        if (userToken) {
+            fetch(`${baseURL}/users/me`, {
+                headers: {
+                    Authorization: `Bearer ${userToken.accessToken}`
+                }
+            }).then(res => {
+                return res.json()
+            }).then(userData => {
+                if (userData.role === "CUSTOMER") {
+                    setIsLoaded(true)
+                } else {
+                    navigate("/access-denied")
+                }
+            })
+        } else {
+            navigate("/access-denied")
+        }
+    }, [pathname])
+
     return (
         <>
-            <TopStrip />
-            <TopBar />
-            <Navbar />
-            <ProfilePageContainer>
-                {props.children}
-            </ProfilePageContainer>
-            <Footer />
+            {isLoaded ?
+                <>
+                    <TopStrip />
+                    <TopBar />
+                    <Navbar />
+                    <ProfilePageContainer>
+                        {props.children}
+                    </ProfilePageContainer>
+                    <Footer />
+                </>
+                :
+                <PreLoader />
+            }
         </>
     )
 }
