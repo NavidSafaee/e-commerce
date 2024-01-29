3796c45f4ce673f674812f5e57a276042fcb4bb1
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2024-01-29 21:55:04 +0330

    Complete ticket part in panel

--- client/src/assets/dashboard/panelPages/Tickets/Tickets.jsx
+++ client/src/assets/dashboard/panelPages/Tickets/Tickets.jsx
@@ -1,69 +1,145 @@
-import React, { useState } from "react";
+import { useContext, useEffect, useState } from "react"
+import s from "./Tickets.module.scss"
+import { isTokenExpired, refreshTokenHandler, showMessage } from "../../../functions";
+import AuthContext from "../../../components/Context/AuthContext";
+import baseURL from "../../../baseURL";
+import PreLoader from "../../../components/PreLoader/PreLoader";
+import { FaPowerOff } from "react-icons/fa"
+import { IoSend } from "react-icons/io5"
+import { RiKakaoTalkFill } from "react-icons/ri"
 
-import { userRows } from "../../datas";
-import "./Tickets.css";
-import DoneIcon from "@mui/icons-material/Done";
-import GradeIcon from "@mui/icons-material/Grade";
 export default function Tickets() {
-  const [userRowsData, setuserRowsData] = useState(userRows);
 
-  const answerTicket = (event) => {
-    document.querySelector("#userTicketBoxAnswer").className =
-      "userTicketBoxAnswer";
-  };
-  const sendTicket = (event) => {
-    document.querySelector("#userTicketBoxAnswer").className =
-      "userTicketBoxAnswerDISACTIVE";
-  };
+  const authContext = useContext(AuthContext)
+
+  const [isContentReady, setIsContentReady] = useState(false)
+  const [ticketList, setTicketList] = useState([])
+  const [showChatBox, setShowChatBox] = useState(false)
+  const [currentChat, setCurrentChat] = useState({})
+  const [newMessage, setNewMessage] = useState("")
+
+  const getAllTickets = () => {
+    const userToken = JSON.parse(localStorage.getItem("userToken"))
+    if (isTokenExpired(userToken.accessToken)) {
+      refreshTokenHandler()
+        .then(token => {
+          authContext.writeTokenInStorage(token)
+          getAllTickets()
+        })
+    } else {
+      fetch(`${baseURL}/tickets/open`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${userToken.accessToken}`
+        }
+      }).then(res => res.json()).then(data => { setTicketList(data); setIsContentReady(true); console.log(data); })
+    }
+  }
+
+  const sendNewMessage = (chat_id) => {
+    const userToken = JSON.parse(localStorage.getItem("userToken"))
+    if (isTokenExpired(userToken.accessToken)) {
+      refreshTokenHandler()
+        .then(token => {
+          authContext.writeTokenInStorage(token)
+          sendNewMessage()
+        })
+    } else {
+      let req_body = {
+        ticketText: newMessage,
+        ticketId: chat_id
+      }
+      fetch(`${baseURL}/tickets/${currentChat._id}/reply`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${userToken.accessToken}`,
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(req_body)
+      }).then(res => {
+        console.log(res);
+        if (res.ok) {
+          setNewMessage("")
+          getAllTickets()
+        } else {
+          showMessage({
+            title: 'Oops!',
+            text: "There was an error sending the message",
+            icon: 'error'
+            // eslint-disable-next-line no-unused-vars
+          })
+        }
+        return res.json()
+      }).then(data => { console.log(data); })
+    }
+  }
+
+  const resolveHandler = 
+
+  useEffect(() => {
+    getAllTickets()
+  }, [])
 
   return (
-    <div className="tickets">
-      <div className="ticketsWrapper">
-        <div className="ticketsBottom">
-          <div className="usertickets">
-            <div className="userTicketBox">
-              <div className="userTcketBoxHearder">
-                <div className="userTcketBoxTitle">
-                  how we can get our orders in oother ciries?
-                </div>
-                <div className="userTicketBoxBtns">
-                  <button className="TicketAnswer" onClick={answerTicket}>
-                    Answer
-                  </button>
-                  <button className="TicketIgnore">Ignore</button>
-                </div>
+    <div className={s.ticketsWrapper}>
+      {!isContentReady && <PreLoader />}
+
+      {isContentReady && <table className={s.ticketsList}>
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>topic</td>
+            <td>date</td>
+            <td>status</td>
+            <td>action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            ticketList?.reverse().map((ticket, i) => (
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{ticket.topic}</td>
+                <td>{ticket.chat[0].date.slice(0, 10)}</td>
+                <td className={`${ticket.status === "OPEN" ? s.open : s.resolved}`}>{ticket.status === "OPEN" ? "Open" : "Closed"}</td>
+                <td>{ticket.status === "OPEN" && (<><RiKakaoTalkFill onClick={() => { setShowChatBox(true); setCurrentChat(ticket) }}/><FaPowerOff onClick={resolveHandler} style={{color: "red", marginLeft: 10}}/></>)}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>}
+
+      {
+        showChatBox && (
+          <div className={s.modal_bg}>
+            <div className={s.close_btn} onClick={() => setShowChatBox(false)}>Close</div>
+            <div className={s.chat_contaner}>
+              <h3 className={s.chat_title}>{currentChat.topic}</h3>
+              <div className={s.messages_filed}>
+                {
+                  currentChat.chat.map((item, i) => (
+                    <div className={`${s.chat_row} ${item.role !== "CUSTOMER" ? s.myMessage : s.customer}`} key={i}>
+                      <div className={s.content}>
+                        {item.chatText}
+                      </div>
+                    </div>
+                  ))
+                }
               </div>
-              <div className="userTicketBoxDes">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates fugiat ducimus quam veniam, aut quibusdam nulla
-                accusamus praesentium animi sequi!
+              <div className={s.type_box}>
+                <input type="text" className={s.input} placeholder="Message" value={newMessage} onChange={e => setNewMessage(e.target.value)} />
+                <button
+                  className={s.send_btn}
+                  onClick={() => {
+                    if (newMessage.trim().length > 0) {
+                      sendNewMessage(currentChat._id)
+                    }
+                  }}><IoSend /></button>
               </div>
-              <div className="userTicketBoxFooter">
-                <div className="TicketDate">2024/1/1</div>
-                <div className="TicketUserId">@sehya</div>
-              </div>
-            </div>
-            <div
-              className="userTicketBoxAnswerDISACTIVE"
-              id="userTicketBoxAnswer"
-            >
-              <div className="userTicketBoxAnswerTitle">
-                how we can get our orders in oother ciries?
-              </div>
-              <div className="userTicketBoxAnswerDes">
-                <textarea
-                  name=""
-                  id="userTicketBoxAnswerDes"
-                  placeholder="Text"
-                ></textarea>
-              </div>
-              <button id="userTicketBoxAnswerBtn" onClick={sendTicket}>
-                SEND
-              </button>
             </div>
           </div>
-        </div>
-      </div>
+        )
+      }
     </div>
-  );
+  )
 }
